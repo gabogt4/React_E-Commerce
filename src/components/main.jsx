836cf2908cd257ea39ad00cefd40417f29bb0b8f
gabogt4 <gabogt4@gmail.com>
2024-09-19 09:13:09 -0500

    Update main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-            src="./assets/main.png.jpg"
+            src="https://www.elrosado.com/Images/Inicio-Pagina-CER-NEW.jpg"
             alt="Card"
             height={500}
           />
@@ -15,9 +15,7 @@ const Home = () => {
             <div className="container">
               <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
               <p className="card-text fs-5 d-none d-sm-block ">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                Siempre de todo a menor precio, SIEMPRE!.
               </p>
             </div>
           </div>
