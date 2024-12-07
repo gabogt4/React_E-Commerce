43284d5f760a8c4f6dc558fe60c7065b138b6bc8
gabogt4 <gabogt4@gmail.com>
2024-12-06 23:50:03 -0500

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -126,7 +126,7 @@ const Products = () => {
       <div className="container my-3 py-3">
         <div className="row">
           <div className="col-12">
-            <h2 className="display-5 text-center">Latest Products</h2>
+            <h2 className="display-5 text-center">Recien llegados</h2>
             <hr />
           </div>
         </div>
